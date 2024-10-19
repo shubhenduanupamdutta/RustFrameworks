4d3962bb2b4f42f96b81e4202d8ea3cafa91cf29
Shubhendu Anupam Dutta <shubhenduanupam@gmail.com>
2024-10-19 21:59:30 +0530

    Basic Tide Server

--- tide_basics/src/main.rs
+++ tide_basics/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+//! Basic Tide Server
+
+async fn hello(req: tide::Request<()>) -> tide::Result {
+    println!("Received request: {:#?}", req);
+    Ok("Hello, Tide!".into())
+}
+
+#[async_std::main]
+async fn main() -> tide::Result<()> {
+    let mut app = tide::new();
+
+    app.at("/hello").get(hello);
+
+    app.listen("127.0.0.1:8000").await?;
+    Ok(())
 }
